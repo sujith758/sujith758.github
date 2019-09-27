8230e884a7a825c21d528f7ee5308ba2e2dd9962
sujith758 <p.sujithk@gmail.com>
2019-09-27 22:00:17 +0530

    pyGroupby

---
+++ src/Groupby.py
@@ -0,0 +1,36 @@
+import sys
+# To ensure that arguments are passed
+if len(sys.argv) != 3:
+    print("Please provide input argument:filename,column1")
+    sys.exit()
+
+#Arguments are passed here <filename><column1>
+arg1 = sys.argv[1]
+arg2 = sys.argv[2]
+
+
+
+# Reading the data in csv file
+with open(arg1, "r") as csvfile:
+    data = csvfile.read()
+lines = data.split()
+# Indexing is given to header line in order to search by header
+header = lines[0].replace("\"", "")
+columns = header.split(",")
+if arg2 != columns[0]:
+    print("Provide column1")
+    sys.exit()
+# print("index of given column", columns.index(arg2))
+columnindex = columns.index(arg2)
+
+# Reading data again to get access to information ignoring header
+with open(arg1, "r") as csvfile:
+    data = csvfile.read().replace("\"", "")
+    lines = data.split("\n")[1:]
+    # List is created and data is loaded into list
+    mylist = list()
+    for line in lines:
+        c1 = line.split(",")[columnindex]
+        mylist.append(c1)
+        # To get count
+    print("count of column", arg2, "is ", len(mylist))
