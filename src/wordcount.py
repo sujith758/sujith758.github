e9750db8eaf151f468829ff5525a6da19cafd550
sujith758 <p.sujithk@gmail.com>
2019-09-27 21:55:15 +0530

    pyWordcount

---
+++ src/wordcount.py
@@ -0,0 +1,33 @@
+import csv, time
+import sys
+import os
+if len(sys.argv) != 2:
+    print("Please provide input argument:filename")
+    sys.exit()
+#Arguments must be added here<filename>
+arg1 = sys.argv[1]
+
+# To get Elapsed time to read given csv file
+def csvReader():
+    with open(arg1,"r") as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=',')
+initialTime = time.time()
+cr = csvReader()
+finalTime = time.time()
+print('The time taken is %s seconds.' % (finalTime - initialTime))
+
+# Current Time and Date
+from datetime import datetime
+timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+print(timestamp)
+
+# To get given file size in bytes
+
+b = os.path.getsize(arg1)
+print(b >> 20)
+
+# To get word count of csv file
+with open(arg1,"r") as tetris:
+    data = tetris.read()
+res = len(data.split("\n"))
+print ("The word count is:" +str(res))
